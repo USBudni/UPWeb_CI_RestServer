d2ba6f5d3df9dc7ccbe166e73be7eb0e54d66508
USBudni <uederbudni@gmail.com>
2017-11-11 16:45:59 -0200

    Controle de token - Menttions

--- application/controllers/api/v1/Mentions.php
+++ application/controllers/api/v1/Mentions.php
@@ -22,66 +22,76 @@ class Mentions extends REST_Controller {
 
     public function Mentions_get()
     {
+        parse_str($_SERVER['QUERY_STRING'], $json);        
+        $this->load->model('Token_model');
+        if ($this->Token_model->verifyToken($json['login'],$json['token'])){
 
-        $id = $this->get('id');
-              
-        // If the id parameter doesn't exist return all the Mentions
+            $id = $this->get('id');
+                
+            // If the id parameter doesn't exist return all the Mentions
 
-        $this->load->model('Mentions_model');   
-        if ($id == NULL)
-        {        
-            $MentionsArray = $this->Mentions_model->getMentions();
-            if ($MentionsArray == NULL)
+            $this->load->model('Mentions_model');   
+            if ($id == NULL)
+            {        
+                $MentionsArray = $this->Mentions_model->getMentions();
+                if ($MentionsArray == NULL)
+                {
+                    // Set the response and exit
+                    $this->response([
+                        'status' => FALSE,
+                        'message' => 'No Mentions were found'
+                    ], REST_Controller::HTTP_NOT_FOUND); // NOT_FOUND (404) being the HTTP response code
+                }
+                else
+                {
+                    $this->response($MentionsArray, REST_Controller::HTTP_OK);
+                }
+            }
+
+            // Find and return a single record for a particular user.
+
+            $id = (int) $id;
+
+            // Validate the id.
+            if ($id <= 0)
             {
-                // Set the response and exit
-                $this->response([
-                    'status' => FALSE,
-                    'message' => 'No Mentions were found'
-                ], REST_Controller::HTTP_NOT_FOUND); // NOT_FOUND (404) being the HTTP response code
+                // Invalid id, set the response and exit.
+                $this->response(NULL, REST_Controller::HTTP_BAD_REQUEST); // BAD_REQUEST (400) being the HTTP response code
+            }
+
+            // Get the user from the array, using the id as key for retrieval.
+            // Usually a model is to be used for this.
+
+            $tag = NULL;
+            $Mentions = $this->Mentions_model->getMentions();        
+            if (!empty($Mentions))
+            {
+                foreach ($Mentions as $key => $value)
+                {
+                    if (isset($value['id']) && (int)$value['id'] === $id)
+                    {
+                        $tag = $value;
+                    }
+                }
+            }
+            
+            if (!empty($tag))
+            {
+                $this->set_response($tag, REST_Controller::HTTP_OK); // OK (200) being the HTTP response code
             }
             else
             {
-                $this->response($MentionsArray, REST_Controller::HTTP_OK);
+                $this->set_response([
+                    'status' => FALSE,
+                    'message' => 'Mention(s) nao encontrada(s)'
+                ], REST_Controller::HTTP_NOT_FOUND); // NOT_FOUND (404) being the HTTP response code
             }
-        }
-
-        // Find and return a single record for a particular user.
-
-        $id = (int) $id;
-
-        // Validate the id.
-        if ($id <= 0)
-        {
-            // Invalid id, set the response and exit.
-            $this->response(NULL, REST_Controller::HTTP_BAD_REQUEST); // BAD_REQUEST (400) being the HTTP response code
-        }
-
-        // Get the user from the array, using the id as key for retrieval.
-        // Usually a model is to be used for this.
-
-        $tag = NULL;
-        $Mentions = $this->Mentions_model->getMentions();        
-        if (!empty($Mentions))
-        {
-            foreach ($Mentions as $key => $value)
-            {
-                if (isset($value['id']) && (int)$value['id'] === $id)
-                {
-                    $tag = $value;
-                }
-            }
-        }
-        
-        if (!empty($tag))
-        {
-            $this->set_response($tag, REST_Controller::HTTP_OK); // OK (200) being the HTTP response code
-        }
+        }        
         else
         {
-            $this->set_response([
-                'status' => FALSE,
-                'message' => 'Mention(s) nao encontrada(s)'
-            ], REST_Controller::HTTP_NOT_FOUND); // NOT_FOUND (404) being the HTTP response code
+            $this->set_response([               
+                'message' => 'Token inválido'
+            ],  REST_Controller::HTTP_BAD_REQUEST);
         }
     }    
 
